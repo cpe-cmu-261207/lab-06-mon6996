c5894aaac89042389734ee445d90b9e5fa17c141
M <thanaporn_chan@cmu.ac.th>
2022-08-11 17:08:40 +0700

    completed

---
+++ pages/components/UserCard.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import {
+    IconChevronDown,
+    IconChevronUp,
+} from "@tabler/icons";
+import UserCardDetail from './UserCardDetail';
+
+export default function UserCard(props) {
+    const [click, setClick] = useState(false);
+
+    return (
+        <div className="border-bottom"
+            onClick={() => { click ? setClick(false) : setClick(true); }}
+        >
+            <div className="d-flex align-items-center p-3">
+                <img src={props.imgUrl}
+                    width="90px"
+                    class="rounded-circle me-4"
+                />
+                <span className="text-center display-6 me-auto">{props.name}</span>
+                {!click && <IconChevronDown />}
+                {click && <IconChevronUp />}
+            </div>
+
+            {
+                click &&
+                <UserCardDetail
+                    email={props.email}
+                    address={props.address}
+                />
+            }
+
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,75 +4,65 @@ import {
   IconMailForward,
   IconMapPins,
 } from "@tabler/icons";
+import { useState } from "react";
+import axios from "axios";
+import UserCard from "./components/UserCard";
 
 export default function Home() {
+  const [count, setCount] = useState('');
+  const [users, setUsers] = useState([]);
   const genUsers = async () => {
-    const resp = await axios.get(`https://randomuser.me/api/`);
+    if (count < 1) {
+      alert("Invalid number of user");
+      return;
+    }
+    const resp = await axios.get(`https://randomuser.me/api/?results=${count}`);
+
+
+    const newUsers = [];
+    for (const x of resp.data.results) {
+      newUsers.push({
+        name: x.name.first + " " + x.name.last,
+        email: x.email,
+        imgUrl: x.picture.large,
+        address: `${x.location.city} ${x.location.state} ${x.location.country} ${x.location.postcode}`
+      });
+    }
+
+    setUsers(newUsers);
   };
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
-      {/* App Header */}
+
       <p className="display-4 text-center fst-italic m-4">
         Multiple Users Generator
       </p>
 
-      {/* Input Section */}
       <div className="d-flex justify-content-center align-items-center fs-5 gap-2">
         Number of User(s)
         <input
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
+          onChange={(event) => { setCount(event.target.value); }}
+          value={count}
         />
         <button class="btn btn-dark" onClick={() => genUsers()}>
           Generate
         </button>
       </div>
 
-      {/* Example of folded UserCard */}
-      <div className="border-bottom">
-        {/* main section */}
-        <div className="d-flex align-items-center p-3">
-          <img
-            src="/profile-placeholder.jpeg"
-            width="90px"
-            class="rounded-circle me-4"
-          />
-          <span className="text-center display-6 me-auto">Name...</span>
-          <IconChevronDown />
-        </div>
+      {users.map((x) => (
+        <UserCard key={x.name}
+          {...x}
+        />
+      ))}
 
-        {/* UserCardDetail is hidden */}
-      </div>
 
-      {/* Example of expanded UserCard */}
-      <div className="border-bottom">
-        {/* main section */}
-        <div className="d-flex align-items-center p-3">
-          <img
-            src="/profile-placeholder.jpeg"
-            width="90px"
-            class="rounded-circle me-4"
-          />
-          <span className="text-center display-6 me-auto">Name...</span>
-          <IconChevronUp />
-        </div>
 
-        {/* UserCardDetail*/}
-        <div className="text-center">
-          <p>
-            <IconMailForward /> Email...
-          </p>
-          <p>
-            <IconMapPins /> Address...
-          </p>
-        </div>
-      </div>
-
-      {/* made by section */}
       <p className="text-center mt-3 text-muted fst-italic">
-        made by Chayanin Suatap 12345679
+        made by Thanaporn Chanchanayothin 640610638
       </p>
     </div>
   );
